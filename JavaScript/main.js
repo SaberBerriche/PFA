5c1365c05e449a432cb46d067623cdaa9e06e17d
SaberBerriche <saberberriche33@gmail.com>
2024-03-31 02:38:53 +0100

    basic files starter

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,12 +1,3 @@
-/*$("#test").click(function(){
-    alert("Clicked");
-});*/
 
-$('#test').click(function(e) {
-    // Function code here
-    console.log('Button clicked!');
-    $(".two.words").hide();
-    console.log(e); // Accessing the event object
-  });
 
 
