9ffc5ed4e6d66ce7c23a51266d81affa0f91c62e
SaberBerriche <saberberriche33@gmail.com>
2024-04-07 05:44:29 +0100

    button animation finished

--- JavaScript/components.js
+++ JavaScript/components.js
@@ -1,3 +1,43 @@
-$("#dark").hover(function(){
-    $(this).css("left","20px");
-})
+
+    var leftvalue = $('#theme_button').width();
+    var widthvalue= $('#light').width();
+    var isLightVisible= true;
+   
+    console.log(leftvalue);
+    leftvalue = leftvalue-widthvalue+1;
+    
+    
+
+    $('.theme').click(function() {
+        if (isLightVisible) {
+          //turn on dark theme
+          $('#theme_button').animate({
+            backgroundColor: 'white'
+          },500);
+
+          $('#light').animate({
+            'left': leftvalue,
+            'opacity': 'hide'
+          }, 500);
+
+          $('#dark').animate({
+            'left': leftvalue
+          }, 500,);
+
+        } else {
+            //turn on light theme
+            $('#theme_button').animate({
+                backgroundColor: 'black'
+              },500);
+          $('#dark').animate({
+            'left': -1
+          }, 500, );
+          $('#light').animate({
+            'left': -1,
+            'opacity': 'show'
+          }, 500);
+        }
+      
+        // Toggle the state
+        isLightVisible = !isLightVisible;
+      });
