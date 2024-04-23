9ef47096541e9b8d1ef806d23d2e7e285a4b98b1
Joyboy5629 <akremhaddad125.com>
2024-04-23 01:12:48 +0100

    hani rbat ben 5edmti w 5edmtek normalmon cv

--- JavaScript/animations.js
+++ JavaScript/animations.js
@@ -1,91 +1,67 @@
 //theme button animation
-var leftvalue = $("#theme_button").width();
-var widthvalue = $("#light").width();
-var isLightVisible = true;
+var leftvalue = $('#theme_button').width();
+var widthvalue= $('#light').width();
+var isLightVisible= true;
 
 console.log(leftvalue);
-leftvalue = leftvalue - widthvalue + 1.6;
+leftvalue = leftvalue-widthvalue+1.6;
 
-$(".theme").click(function () {
-  if (isLightVisible) {
-    //turn on dark theme
-    $("#theme_button").animate(
-      {
-        backgroundColor: "white",
-      },
-      500
-    );
 
-    $("#light").animate(
-      {
-        left: leftvalue,
-        opacity: "hide",
-      },
-      500
-    );
 
-    $("#dark").animate(
-      {
-        left: leftvalue,
-      },
-      500
-    );
-  } else {
-    //turn on light theme
-    $("#theme_button").animate(
-      {
-        backgroundColor: "black",
-      },
-      500
-    );
-    $("#dark").animate(
-      {
-        left: -1.6,
-      },
-      500
-    );
-    $("#light").animate(
-      {
-        left: -1.6,
-        opacity: "show",
-      },
-      500
-    );
-  }
+$('.theme').click(function() {
+    if (isLightVisible) {
+        
+      //turn on dark theme
+      $('#theme_button').animate({
+        backgroundColor: 'white'
+      },500);
 
-  // Toggle the state
-  isLightVisible = !isLightVisible;
-});
+      $('#light').animate({
+        'left': leftvalue,
+        'opacity': 'hide'
+      }, 500);
 
-//calendar animation
-$("#cal_box").hover(
-  function () {
-    $("#calend_icon").animate({
-      padding: "10px",
-      "background-color": "rgb(131,148,163)",
-    });
-  },
-  function () {
-    $("#calend_icon").stop();
-    $("#calend_icon").animate({
-      padding: "0px",
-      "background-color": "rgb(82,119,153)",
-    });
-  }
-);
-// support anim
-$("#sup_box").hover(
-  function () {
-    $("#sup_icon").animate({
-      padding: "10px",
-      "background-color": "rgb(131,148,163)",
-    });
-  },
-  function () {
-    $("#sup_icon").stop();
-    $("#sup_icon").animate({
-      padding: "0px",
-      "background-color": "rgb(82,119,153)",
-    });
-  }
-);
+      $('#dark').animate({
+        'left': leftvalue
+      }, 500,);
+
+    } else {
+        
+        //turn on light theme
+        $('#theme_button').animate({
+            backgroundColor: 'black'
+          },500);
+      $('#dark').animate({
+        'left': -1.6
+      }, 500, );
+      $('#light').animate({
+        'left': -1.6,
+        'opacity': 'show'
+      }, 500);
+    }
+  
+    // Toggle the state
+    isLightVisible = !isLightVisible;
+  });
+
+
+  //icons animation
+  $(".icons").hover(
+    function() {
+      $(this).animate({
+        'padding': '10px',
+        'background-color' : 'rgb(131,148,163)'
+      });
+      
+    },
+    function() {
+      $(this).stop();
+      $(this).animate({
+        'padding': '0px',
+        'background-color' : 'rgb(82,119,153)'
+      });
+    }
+  );
+   
+ 
+  
